c368cac7993da4a7134682636a3c6ca9b96854de
Prashant G <prashant.gaikwad@leftrightmind.com>
2018-11-19 14:47:59 +0530

    Complete code

--- Binary Search/ViewController.swift
+++ Binary Search/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let numbers = [1, 4, 6, 7, 10, 12, 18, 19, 23, 24, 34, 44, 58, 65]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print(binarySearch(array: numbers, key: 34))
     }
 
+    func binarySearch(array: [Int], key: Int) -> Bool {
+        
+        if array.count == 0 { return false }
+        
+        let minIndex = 0
+        let maxIndex = array.count - 1
+        let midIndex = maxIndex/2
+        let midValue = array[midIndex]
+        
+        if key < array[minIndex] || key > array[maxIndex] {
+            print("\(key) is not in the array")
+            return false
+        }
+        
+        if key > midValue {
+            let slice = Array(array[midIndex + 1...maxIndex])
+            return binarySearch(array: slice, key: key)
+        }
+        
+        if key < midValue {
+            let slice = Array(array[minIndex...midIndex - 1])
+            return binarySearch(array: slice, key: key)
+        }
+        
+        if key == midValue {
+            print("\(key) found in the array")
+            return true
+        }
+        
+        return false
+    }
 
 }
 
